7c23c0dd3e56f962b43eabeafdfacb48201efe48
Admin <vasay1805@yandex.ru>
2022-02-17 10:04:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import telebot
+from telebot import types
+from mes import *
+
+
+token = ""
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    # Первое сообщение бота
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add('/help')
+    bot.send_message(message.chat.id, information.start, reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add('/abitur', '/contacts', '/mtuci')
+    bot.send_message(message.chat.id, information.help, reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['abitur'])
+def abitur(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add('/help')
+    bot.send_message(message.chat.id, information.abitur, reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['contacts'])
+def contacts(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add('/help')
+    bot.send_message(message.chat.id, information.contacts, reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['mtuci'])
+def mtuci(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add('/help')
+    bot.send_message(message.chat.id, information.mtuci, reply_markup=keyboard)
+
+@bot.message_handler(commands=['menu'])
+def menu(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add('/help')
+    bot.send_message(message.chat.id, information.menu, reply_markup=keyboard)
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    if message.text.lower() == 'привет':
+        start(message)
+    if message.text.lower() == 'спишь?':
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add('/help')
+        bot.send_message(message.chat.id, information.answer1, reply_markup=keyboard)
+    if message.text.lower() == 'пинг':
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add('/help')
+        bot.send_message(message.chat.id, information.answer2, reply_markup=keyboard)
+    if message.text.lower() == 'да':
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add('/help')
+        bot.send_message(message.chat.id, information.answer3, reply_markup=keyboard)
+
+
+bot.polling(none_stop=True)
